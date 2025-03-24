8ff7c7a6a949721f94cf1e0c58de36dd4bc13b2e
mirabbos.egamberdiye <mirabbosegamberdiyevgmail.com>
2025-03-24 09:26:50 +0500

    discovery-server qo'shildi

--- product-service/src/main/java/com/programmingtechie/product_service/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.programmingtechie.product_service.repository;
-
-import com.programmingtechie.product_service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository  extends MongoRepository<Product, String> {
-}
